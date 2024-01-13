fa2173eed05aad8fcce55b38dc1a80b074352f66
Keno Bacunawa <renzpalonpon@gmail.com>
2024-01-13 18:31:50 +0800

    setup

---
+++ src/types/TableDefaultOptions.ts
@@ -0,0 +1,10 @@
+interface TableDefaultOptions {
+  page: number;
+  itemsPerPage: number;
+  search?: string;
+  category_id?: number;
+  quiz_id?: number;
+  first?: number;
+}
+
+export default TableDefaultOptions;
